bacd5eaba25382ae3a72464863357de66b7e08fb
nurimeansworld <nurimeansworld@gmail.com>
2024-05-25 18:42:41 +0900

    init: 테스트 파일 삭제 및 뼈대 추가

--- yearend/src/App.js
+++ yearend/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <header className='App-header'>header 입니당</header>
+      <section>
+        <h1>2024YearEnd✨</h1>
+      </section>
+      <footer>footer 입니당</footer>
     </div>
   );
 }
