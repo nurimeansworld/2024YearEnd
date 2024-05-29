bec1c56efceaeaef2022d4d0776c8354c08d541a
nurimeansworld <nurimeansworld@gmail.com>
2024-05-29 20:14:02 +0900

    Add: GlobalStyle, 디폴트 색상들 설정 #1

--- yearend/src/App.js
+++ yearend/src/App.js
@@ -1,12 +1,18 @@
+import GlobalStyle from 'GlobalStyle';
+
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>header 입니당</header>
-      <section>
-        <h1>2024YearEnd✨</h1>
-      </section>
-      <footer>footer 입니당</footer>
-    </div>
+    <>
+      <GlobalStyle />
+
+      <div className='App'>
+        <header className='App-header'>header 입니당</header>
+        <section>
+          <h1>2024YearEnd✨</h1>
+        </section>
+        <footer>footer 입니당</footer>
+      </div>
+    </>
   );
 }
 
