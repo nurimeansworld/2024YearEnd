ed180cb0379b8ad7102812818a14ec1092b06a37
nurimeansworld <nurimeansworld@gmail.com>
2024-06-06 19:17:15 +0900

    Add: 메인 페이지 필요한 컴포넌트 생성 및 스타일 작업(WIP) #1

---
+++ yearend/src/components/Share.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+function Share() {
+  return (
+    <div>
+      결과 공유하기
+      <ul>
+        <li>- 이미지 저장하기</li>
+        <li>- 링크 복사하기</li>
+        <li>- Instagram</li>
+        <li>- Facebook</li>
+        <li>- KakaoTalk</li>
+      </ul>
+    </div>
+  );
+}
+export default Share;
