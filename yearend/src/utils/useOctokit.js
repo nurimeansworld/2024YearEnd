e7d3cc0f9ee2d7d842bad14682eeb7b31b5a73d7
nurimeansworld <nurimeansworld@gmail.com>
2024-09-24 17:46:54 +0900

    Add: 각 데이터 count 값 받아오는 getAllCounts 추가 #3

--- yearend/src/utils/useOctokit.js
+++ yearend/src/utils/useOctokit.js
@@ -2,14 +2,14 @@ import { useState, useEffect } from 'react';
 import { octokit } from 'utils/octokit';
 
 function useOctokit(username, repo, url) {
-  const [user, setUser] = useState(null);
+  const [data, setData] = useState(null);
+  const [err, setErr] = useState(false);
+  const [loading, isLoading] = useState(false);
 
   const getUserData = async () => {
+    isLoading(false);
     try {
       const res = await octokit.request('GET {url}', {
-        // const res = await octokit.request('GET /users/{account_id}', {
-        // const res = await octokit.request('GET /users/{account_id}/repos?type=all', {
-        // const res = await octokit.request('GET /repos/{account_id}/{repo}/commits', {
         account_id: username,
         repo: repo,
         url: url,
@@ -19,29 +19,36 @@ function useOctokit(username, repo, url) {
         headers: { 'X-GitHub-Api-Version': '2022-11-28' },
       });
 
-      setUser(res.data);
+      setData(res.data);
+      const counts = getAllCounts(res);
 
-      // const headerString = res ? res.headers.link : '';
-      const headerString = res.headers.link;
-      const numOfPage = headerString.split(',')[1];
-      const counts = res.headers.link
-        ? numOfPage
-            .match(/&page=[0-9]*[>&]/)
-            .join('')
-            .replace(/[^0-9]/g, '')
-        : 0;
-
-      setUser({ ...res.data, counts: counts });
+      setData({ ...res.data, counts: counts });
     } catch (err) {
-      console.error(err);
+      // console.error(err);
+      setErr(err);
     }
+    isLoading(true);
   };
 
   useEffect(() => {
     getUserData();
   }, []);
 
-  return user;
+  return data;
 }
+
+const getAllCounts = (res) => {
+  // const headerString = res ? res.headers.link : '';
+  const headerString = res.headers.link;
+  const numOfPage = headerString.split(',')[1];
+  const counts = res.headers.link
+    ? numOfPage
+        .match(/&page=[0-9]*[>&]/)
+        .join('')
+        .replace(/[^0-9]/g, '')
+    : 0;
+
+  return counts;
+};
 
 export default useOctokit;
