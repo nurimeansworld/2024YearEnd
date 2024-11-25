f3bf603a89d61ddc6ffd3a0a96c525edafe79f73
nurimeansworld <nurimeansworld@gmail.com>
2024-11-25 16:56:05 +0900

    pack: router 설정을 위한 react-router-dom 설치 #4

--- yearend/src/App.js
+++ yearend/src/App.js
@@ -1,12 +1,20 @@
 import GlobalStyle from 'GlobalStyle';
 import MainPage from 'pages/MainPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <GlobalStyle />
 
-      <MainPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<MainPage />} />
+          <Route path='/result' element={<MainPage />} />
+        </Routes>
+      </BrowserRouter>
+
+      {/* <MainPage /> */}
     </>
   );
 }
