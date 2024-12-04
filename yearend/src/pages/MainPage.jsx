0a730a9b27ed262038d0ab57a839d082ae7d1ce8
nurimeansworld <nurimeansworld@gmail.com>
2024-12-04 17:01:32 +0900

    Add: 메인 페이지 ENTER 버튼 추가 #16

--- yearend/src/pages/MainPage.jsx
+++ yearend/src/pages/MainPage.jsx
@@ -1,47 +1,44 @@
 import styled from 'styled-components';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { YEAR } from 'utils/constants';
 import { validateID, checkExistID } from 'utils/functions';
+// import { Title } from 'components/layout';
 
 function MainPage() {
   const navigate = useNavigate();
   const [userID, setUserID] = useState('');
 
   const handleEnter = async (e) => {
-    if (e.key === 'Enter') {
+    if (e.key === 'Enter' || e.type === 'click') {
       // 1. 유효성 검사
-      const errorMessage = validateID(e.target.value);
+      const errorMessage = validateID(userID);
       if (errorMessage) {
         alert(errorMessage);
         return;
       }
 
       // 2. username 실존 검사
-      const checkUser = await checkExistID(e.target.value);
-      return !checkUser
-        ? alert('유효한 ID가 아닙니다.')
-        : setUserID(e.target.value);
+      const checkUser = await checkExistID(userID);
+      return !checkUser ? alert('유효한 ID가 아닙니다.') : handleNavigate();
     }
   };
-  const handleTimeout = () => {
+  const handleNavigate = () => {
+    // CHECK:: 효과음 넣기
     setTimeout(() => {
       navigate('/result', { state: userID });
-    }, 5000);
+    }, 1000);
   };
-
-  useEffect(() => {
-    userID && handleTimeout();
-  }, [userID]);
 
   return (
     <>
+      {/* <Title /> */}
       {/* 1 - intro */}
       <Intro>
         <h2 className='sr-only'>연말결산 소개</h2>
-        <p>GitHub로 돌아보는 나의 {YEAR}년 개발 기록</p>
-        <p>
+        <p id='writing1'> GitHub로 돌아보는 나의 {YEAR}년 개발 기록</p>
+        <p id='writing2'>
           계속하려면 아무 키나 누르십시오. . .
           <br />
           (Press any key to continue. . .)
@@ -58,26 +55,19 @@ function MainPage() {
             name='username'
             placeholder='영문 or 숫자 조합'
             maxLength='40'
+            value={userID}
+            onChange={(e) => setUserID(e.target.value)}
             onKeyDown={handleEnter}
           />
         </p>
-        <button>ㄴEnter</button>
-        {userID && (
-          <>
-            <p>
-              입력하신 아이디(username)는 '<span id='userName'>{userID}</span>'
-              입니다.
-            </p>
-            <p>5초 뒤 이동합니다 . . .</p>
-          </>
-        )}
+        <button type='button' onClick={handleEnter}>
+          ENTER
+        </button>
       </Form>
-
       {/* 2 - result */}
     </>
   );
 }
-
 const Intro = styled.section`
   p {
     position: relative;
@@ -132,6 +122,15 @@ const Form = styled.section`
   #userName {
     border-width: 0 0 1px;
     border-style: dashed;
+  }
+  button {
+    color: inherit;
+    font-size: 2rem;
+    text-align: center;
+    display: block;
+    margin: 0 auto;
+    border: 1px solid;
+    padding: 1rem 3rem;
   }
 `;
 
