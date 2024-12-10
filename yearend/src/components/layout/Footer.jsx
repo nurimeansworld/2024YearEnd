bc319e8366e0d131c106fd34cb8ba48f54b5d074
nurimeansworld <nurimeansworld@gmail.com>
2024-12-10 15:56:14 +0900

    Add: 로고 이미지 저작권 표시

--- yearend/src/components/layout/Footer.jsx
+++ yearend/src/components/layout/Footer.jsx
@@ -7,6 +7,13 @@ function Footer() {
         <li>만든이 : nurimeansworld@gmail.com</li>
         <li>관련 문의사항은 해당 메일로 부탁드립니다.</li>
       </ul>
+      <a
+        href='https://www.flaticon.com/kr/free-icons/-'
+        title='소설 속의 인물 아이콘'
+        className='sr-only'
+      >
+        소설 속의 인물 아이콘 제작자: Freepik - Flaticon
+      </a>
     </WrapFooter>
   );
 }
